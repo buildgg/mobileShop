884e39d72a4fa65937ddee53e73f48fcd1ce73a4
buildgg <popov.m.96@gmail.com>
2017-01-13 01:58:55 +0200

    Create UriRequest.java

---
+++ src/main/java/ua/home/mobileshop/util/UriRequest.java
@@ -0,0 +1,25 @@
+package ua.home.mobileshop.util;
+
+public class UriRequest {
+
+    public static boolean isStaticUrl(String uri) {
+        return uri.startsWith("/static/");
+    }
+    public static boolean isMediaUrl(String uri) {
+        return uri.startsWith("/media/");
+    }
+    public static boolean isAjaxUrl(String url) {
+        return url.startsWith("/ajax/");
+    }
+
+    public static boolean isAjaxJsonUrl(String url) {
+        return url.startsWith("/ajax/json/");
+    }
+
+    public static boolean isAjaxHtmlUrl(String url) {
+        return url.startsWith("/ajax/html/");
+    }
+    private UriRequest(){
+
+    }
+}
